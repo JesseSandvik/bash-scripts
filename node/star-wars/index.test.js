459985aacd5efdb68c73f157fae4729d90513e9f
Jesse <sandvikjesse@gmail.com>
2023-01-16 18:20:47 -0500

    smoke test for Jest passing

---
+++ node/star-wars/index.test.js
@@ -0,0 +1,7 @@
+"use strict";
+describe("Smoke Test", () => {
+    test("This should pass regardless", () => {
+        const string = "smoke test";
+        expect(string).toEqual("smoke test");
+    });
+});
---
+++ node/star-wars/index.test.ts
@@ -0,0 +1,9 @@
+
+
+describe("Smoke Test", () => {
+    test("This should pass regardless", () => {
+        const string="smoke test";
+
+        expect(string).toEqual("smoke test");
+    });
+});
